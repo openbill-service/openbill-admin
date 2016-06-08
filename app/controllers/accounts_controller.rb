409e5598cc25e2f84f20d0feb400a7c4195f7ab0
Danil Pismenny <danil@brandymint.ru>
2016-06-08 17:33:22 +0300

    paginate account transactions

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -44,9 +44,7 @@ class AccountsController < ApplicationController
   private
 
   def transactions
-    Openbill.service.account_transactions(account).reverse_order(:created_at).map do |t|
-      AccountTransaction.new account, t
-    end
+    Openbill.service.account_transactions(account).reverse_order(:created_at).paginate(page, per_page)
   end
 
   def account
