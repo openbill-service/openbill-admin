9ef031bb8d6f99497d0da93cf3127687a1e074f5
Danil Pismenny <danil@brandymint.ru>
2016-06-08 18:28:27 +0300

    add nprogress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+NProgress.configure({
+  showSpinner: false,
+  ease: 'ease',
+  speed: 500
+});
