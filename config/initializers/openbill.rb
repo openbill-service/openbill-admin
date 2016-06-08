28107ffeaca9f3352d97ce1692e429e380cd5ed8
Danil Pismenny <danil@brandymint.ru>
2016-06-08 11:01:06 +0300

    configure sequel max_connections

--- config/initializers/openbill.rb
+++ config/initializers/openbill.rb
@@ -1,7 +1,7 @@
 begin
   Openbill.configure do |config|
     config.default_currency = 'RUB'
-
+    config.max_connections = 10
     config.database = YAML.load(ERB.new(IO.read('./config/database.yml')).result)[Rails.env].symbolize_keys
   end
 rescue Sequel::DatabaseError => err
