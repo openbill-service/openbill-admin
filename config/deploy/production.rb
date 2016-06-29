8e6cdbace478339499d6ff7da072305a1effd8fb
Denis Khabarov <admin@saymon21-root.pro>
2016-06-29 13:57:38 +0400

    fix restart (#35)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :application, 'billing.kiiiosk.ru'
 set :stage, :production
-set :unicorn_restart_cmd, "service unicorn-#{fetch(:application)} upgrade"
+set :unicorn_restart_cmd, "/etc/init.d/unicorn-#{fetch(:application)} upgrade"
 set :repo_url, 'git@github.com:openbill-service/openbill-admin.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, -> { "/home/wwwkiiiosk/#{fetch(:application)}" }
