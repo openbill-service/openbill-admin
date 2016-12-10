9b65032edf756c3ed5ca2a1ab70e616ac00e03e6
Danil Pismenny <danil@brandymint.ru>
2016-12-10 21:42:50 +0300

    Отключил srv-1 в deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :unicorn_restart_cmd, "/etc/init.d/unicorn-#{fetch(:application)} upgrade"
 set :repo_url, 'git@github.com:openbill-service/openbill-admin.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, -> { "/home/wwwkiiiosk/#{fetch(:application)}" }
-server 'srv-1.kiiiosk.ru', user: 'wwwkiiiosk', port: 22, roles: %w(web app)
+# server 'srv-1.kiiiosk.ru', user: 'wwwkiiiosk', port: 22, roles: %w(web app)
 server 'srv-2.kiiiosk.ru', user: 'wwwkiiiosk', port: 22, roles: %w(web app)
 set :rails_env, :production
 set :branch, ENV['BRANCH'] || 'master'
