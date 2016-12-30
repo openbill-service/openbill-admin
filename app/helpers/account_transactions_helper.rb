79f7348d6e1e32b74de0d5affd992e65e069d1ca
Danil Pismenny <dapi@users.noreply.github.com>
2016-12-30 14:09:45 +0300

    accounts table (#61)

--- app/helpers/account_transactions_helper.rb
+++ app/helpers/account_transactions_helper.rb
@@ -1,4 +1,18 @@
 module AccountTransactionsHelper
+  def amount_by_month(account, month)
+    plus = Openbill.service.transactions
+      .where(to_account_id: account.id)
+      .where('created_at >= ? and created_at <?', month.beginning_of_month, month.end_of_month)
+      .sum(:amount_cents) || 0
+    minus = Openbill.service.transactions
+      .where(from_account_id: account.id)
+      .where('created_at >= ? and created_at <?', month.beginning_of_month, month.end_of_month)
+      .sum(:amount_cents) || 0
+
+
+    Money.new plus - minus, account.amount_currency
+  end
+
   def accounts_count(cat = nil)
     if cat.present?
       Openbill.service.accounts.where(category_id: cat.id).count
