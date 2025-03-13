dda720590f3b81f60c60f40b2b7e5612f563c1e7
Danil Pismenny <danil@brandymint.ru>
2025-03-13 16:40:11 +0300

    Add importmap.rb

---
+++ config/importmap.rb
@@ -0,0 +1,12 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/channels", under: "channels"
+pin_all_from "app/javascript/elements", under: "elements"
+pin_all_from "app/javascript/helpers", under: "helpers"
+pin "bootstrap", to: "bootstrap.bundle.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
